674b8a32e451a27c85ab9ca3743184eac23a4aa0
we dul <wedul.chul@gmail.com>
2019-06-26 22:31:44 +0900

    vue 초기 생성

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# vue_study
-for vue.js study
+# study
 
+> vue study
 
-vue js study 파일입니당
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ index_single_file_component.vue
@@ -0,0 +1,26 @@
+<!-- 싱글 파일 컴포넌트로 vue 파일로 프로젝트 구조를 구성한 파일로 vue파일 한개당 1개의 컴포넌트를 의미. -->
+
+<!-- html 파일 입력 -->
+<template>
+    <div>
+        <span>
+            <button>{{message}}</button>
+        </span>
+    </div>
+</template>
+
+<!-- 뷰 컴포넌트의 내용을 정의하는 영역 (template, data, method 등등..) -->
+<script>
+export default {
+    data: {
+        message: 'click this button'
+    }
+}    
+</script>
+
+
+<style>
+span {
+    font-size: 1.2em;
+}
+</style>
