f6f0d21abc13ff525127aee5f11a7edd3ec1a806
we dul <wedul.chul@gmail.com>
2019-06-29 01:08:47 +0900

    modal 추가

--- components/TodoInput.vue
+++ components/TodoInput.vue
@@ -4,14 +4,25 @@
         <span class="addContainer" v-on:click="addTodo">
             <i class="addBtn fas fa-plus" aria-hidden="true"></i>
         </span>
+
+        <modal v-if="showModal" @close="showModal=false">
+            <h3 slot="header">경교</h3>
+            <span slot="footer" @click="showModal = false">
+                할 일을 입력하세요.
+                <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+            </span>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from '../common/Modal';
+
 export default {
     data() {
         return {
-            newTodoItem: ''
+            newTodoItem: '',
+            showModal: false
         }
     },
     methods: {
@@ -19,8 +30,13 @@ export default {
             if (this.newTodoItem !== '') {
                 const value = this.newTodoItem && this.newTodoItem.trim();
                 this.$emit('addTodo', value);
+            } else {
+                this.showModal = !this.showModal;
             }
         }
+    },
+    components: {
+        Modal: Modal
     }
 }
 </script>
