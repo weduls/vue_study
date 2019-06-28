8027ab92e215f734387577a2b0f046dc45e1567c
we dul <wedul.chul@gmail.com>
2019-06-28 23:32:50 +0900

    컴포넌트 등록

---
+++ components/TodoFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>footer</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ components/TodoInput.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>input</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
